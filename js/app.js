f5c24aec4cbc60794c7d0e35761732c91f9adaec
prestonevans <89256614+prestonevans@users.noreply.github.com>
2022-08-15 14:24:17 -0600

    made basic sampler using audio elements

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const audioElements = document.querySelectorAll('audio');
+const audioButtons = document.querySelectorAll('.audio');
+
+audioButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    audioElements.forEach((audioSample) => {
+      audioSample.pause();
+      audioSample.currentTime = 0;
+    });
+    e.target.children[0].play();
+  });
+});
